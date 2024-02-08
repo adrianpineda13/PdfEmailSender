a46500d20ade6c11a59716c304a540e726988bf5
Luis Pineda <luis.pineda@wbd.com>
2024-02-08 11:30:33 -0600

    appsettings file added, able to read values from it

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateApplicationBuilder(args).Build();
+
+IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+
+int keyOneValue = config.GetValue<int>("KeyOne");
+bool keyTwoValue = config.GetValue<bool>("KeyTwo");
+string? keyThreeNestedValue = config.GetValue<string>("KeyThree:Message");
+
+
+Console.WriteLine($"KeyOne = {keyOneValue}");
+Console.WriteLine($"KeyTwo = {keyTwoValue}");
+Console.WriteLine($"KeyThree:Message = {keyThreeNestedValue}");
+
+await host.RunAsync();
